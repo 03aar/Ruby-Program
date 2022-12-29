0e612d2ffe5df4a3f973f869fa349750959f10ba
AARON K <96762156+03aar@users.noreply.github.com>
2022-12-29 22:05:16 +0530

    Add files via upload

---
+++ getvaluewithkey.rb
@@ -0,0 +1,9 @@
+#Get value using key
+city_info = {
+    "name" => "San Francisco",
+    "country" => "USA",
+    "zip" => "94131"
+}
+
+puts city_info['name'] + ", " + city_info['country']
+puts city_info['zip']
